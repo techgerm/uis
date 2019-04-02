cedfd81500f295f96974d9a5ed95ded3e2cbca4e
German Pineda <germanp173@gmail.com>
2019-04-01 23:23:06 -0700

    Implement burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+import AppMenu from './components/AppMenu/AppMenu';
 import "./App.css";
+import { render } from 'react-dom';
+import { Menu } from 'antd';
 
 function App() {
-  return (
-    <div>
-      <button>TESTING</button>
-    </div>
-    // <Router>
-    //   <div>
-    //     <Switch>
 
-    //     </Switch>
-    //   </div>
-    // </Router>
+  return (
+    <Router>
+      <div>
+        <AppMenu />
+        <Switch>
+          <Route exact component={Home} path="/" />
+          <Route exact component={Contact} path="/contact" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
