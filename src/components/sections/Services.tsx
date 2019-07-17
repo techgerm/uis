6611007d1a2e668bb75941ea164f5749eed9d374
unknown <pinedavictor095@gmail.com>
2019-07-16 23:48:01 -0700

    added item component and updated Services.tsx

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Item } from "../services/Item";
 
 export class Services extends React.Component {
 	render() {
@@ -11,124 +11,33 @@ export class Services extends React.Component {
 						<h2>Our Services</h2>
 						<div className="bar" />
 					</div>
-
 					<div className="uk-grid uk-grid-match uk-grid-medium uk-child-width-1-3@m uk-child-width-1-2@s">
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-plan" />
-								</div>
-								<h3>Digital Branding</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-think" />
-								</div>
-								<h3>Creative Solutions</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-shout" />
-								</div>
-								<h3>Marketing Solutions</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-analytics-1" />
-								</div>
-								<h3>Business Analytics</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services active">
-								<div className="icon">
-									<i className="flaticon-ux-design" />
-								</div>
-								<h3>UX & UI Design</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-camera" />
-								</div>
-								<h3>Photography</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-project" />
-								</div>
-								<h3>Management</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-chat" />
-								</div>
-								<h3>Social Media</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
-
-						<div className="item">
-							<div className="single-services">
-								<div className="icon">
-									<i className="flaticon-data" />
-								</div>
-								<h3>Data Science</h3>
-
-								<i className="flaticon-right link-btn" />
-
-								<Link to="#" className="link uk-position-cover" />
-							</div>
-						</div>
+						<Item itemTitle="Affidavits" itemIcon="flaticon-plan"></Item>
+						<Item
+							itemTitle="Business Contracts"
+							itemIcon="flaticon-think"
+						></Item>
+						<Item
+							itemTitle="Power of Attorney"
+							itemIcon="flaticon-shout"
+						></Item>
+						<Item
+							itemTitle="Apostille Services"
+							itemIcon="flaticon-analytics-1"
+						></Item>
+						<Item itemTitle="Live Scan" itemIcon="flaticon-ux-design"></Item>
+						<Item
+							itemTitle="Document Translation"
+							itemIcon="flaticon-camera"
+						></Item>
+						<Item
+							itemTitle="Wills and Living Trusts"
+							itemIcon="flaticon-project"
+						></Item>
+						<Item
+							itemTitle="Documentos y Trãmites Legales en Colombia"
+							itemIcon="flaticon-chat"
+						></Item>
 					</div>
 				</div>
 			</section>
--- src/components/services/item.tsx
+++ src/components/services/item.tsx
@@ -1,1 +1,26 @@
 import React from "react";
+/* TODO: Future enhancement for pricing model */
+// import { Link } from "react-router-dom";
+
+interface ItemState {
+	itemTitle: string;
+	itemIcon: string;
+}
+
+export class Item extends React.Component<any, ItemState> {
+	render() {
+		return (
+			<div className="item">
+				<div className="single-services">
+					<div className="icon">
+						<i className={this.props.itemIcon} />
+					</div>
+					<h3>{this.props.itemTitle}</h3>
+					{/* TODO: Future enhancement for pricing model */}
+					{/* <i className="flaticon-right link-btn" /> */}
+					{/* <Link to="#" className="link uk-position-cover" /> */}
+				</div>
+			</div>
+		);
+	}
+}
