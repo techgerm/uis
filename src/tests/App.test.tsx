5dfc2709eafa165aa402b1d0814d2ecce0ae2c75
German Pineda <germanp173@gmail.com>
2019-09-03 02:06:23 -0700

    Add type-check script and run this script instead of full build

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "../App";
 
-// global.scrollTo = jest.fn();
-
 it("renders without crashing", () => {
 	const div = document.createElement("div");
 	ReactDOM.render(<App />, div);
